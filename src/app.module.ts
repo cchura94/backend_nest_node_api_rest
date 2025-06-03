9218e1dd59fd63de06cdf2615d6c6dd46bcfb163
cristian <cchura.cpc@gmail.com>
2025-06-02 22:38:31 -0400

    base de datos + docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: '.development.env'
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5436, // el puerto pueden cambiar
+      username: 'postgres',
+      password: 'admin54321',
+      database: 'bd_backend_nest2',
+      entities: [],
+      synchronize: false
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
