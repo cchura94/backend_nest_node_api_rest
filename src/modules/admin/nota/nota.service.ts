4ae5e8396eb5c474eedfdf502fa87f8fc754e372
cristian <cchura.cpc@gmail.com>
2025-06-26 14:11:54 -0400

    nota y movimiento + actualización de stock

--- src/modules/admin/nota/nota.service.ts
+++ src/modules/admin/nota/nota.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateNotaDto } from './dto/create-nota.dto';
 import { UpdateNotaDto } from './dto/update-nota.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -9,6 +9,7 @@ import { User } from '../users/entities/user.entity';
 import { Producto } from '../inventario/producto/entities/producto.entity';
 import { Almacen } from '../inventario/almacen/entities/almacen.entity';
 import { EntidadComercial } from '../entidad-comercial/entities/entidad-comercial.entity';
+import { AlmacenProducto } from '../inventario/almacen/entities/almacen_producto.entity';
 
 @Injectable()
 export class NotaService {
@@ -26,6 +27,9 @@ export class NotaService {
     private almacenRepo: Repository<Almacen>,
     @InjectRepository(EntidadComercial)
     private entidadRepo: Repository<EntidadComercial>,
+    @InjectRepository(AlmacenProducto)
+    private almacenProductoRep: Repository<AlmacenProducto>,
+    
     
   ){
 
@@ -43,29 +47,76 @@ export class NotaService {
 
     // crear la nota
     const nota  = await this.notaRepo.create({
-      ...CreateNotaDto,
+      ...createNotaDto,
       entidad_comercial: entidad,
-      user: user,
-      movimientos: []
+      user: user
     });
+
+    // guardar la nota primero para obtner el ID para movimientos
+    await this.notaRepo.save(nota);
+
+    const movimientosGuardados: Movimiento [] = [];
 
     for (const m of createNotaDto.movimientos) {
       const producto = await this.productoRepo.findOneBy({id: m.producto_id});
       if(!producto) throw new NotFoundException('Producto no encontrado')
 
-        const almacen = await this.almacenRepo.findOneBy({id: m.producto_id});
+        const almacen = await this.almacenRepo.findOneBy({id: m.almacen_id});
         if(!almacen) throw new NotFoundException('Almacen no encontrado');
 
         const movimiento = this.movRepo.create({
           ...m,
+          nota: nota,
           producto,
           almacen
         });
 
-        nota.movimientos.push(movimiento);  
+        await this.actualizarStock(almacen, producto, m.cantidad, m.tipo_movimientos);
+
+        const movGuardado = await this.movRepo.save(movimiento)
+        movimientosGuardados.push(movGuardado);
+       //  nota.movimientos.push(movimiento);  
     }
 
-    return await this.notaRepo.save(nota);
+    nota.movimientos= movimientosGuardados;
+
+    //const notaaa = await this.notaRepo.save(nota);
+    return nota;
+  }
+
+  private async actualizarStock(almacen: Almacen, producto: Producto, cantidad: number, tipo: 'ingreso' | 'salida' | 'devolucion'){
+
+    let ap = await this.almacenProductoRep.findOne({
+      where: {
+        almacen: {id: almacen.id},
+        producto: {id: producto.id}
+      },
+      relations: ['almacen', 'producto']
+    });
+
+    if(!ap){
+      if(tipo === 'salida'){
+        throw new BadRequestException(`No hay stock registrado para este producto en este almacen`);
+      }
+
+      ap = this.almacenProductoRep.create({
+        almacen, producto, cantidad_actual: cantidad, fecha_actualizacion: new Date()
+      });
+
+    }else{
+      if(tipo === 'ingreso' || tipo === 'devolucion'){
+        ap.cantidad_actual += cantidad;
+      }else if(tipo === 'salida'){
+        if(ap.cantidad_actual < cantidad) {
+          throw new BadRequestException(`Stock insuficiente para la salida`);
+        }
+        ap.cantidad_actual -= cantidad;
+      }
+      ap.fecha_actualizacion = new Date()
+    }
+
+    await this.almacenProductoRep.save(ap);
+
   }
 
   findAll() {
@@ -73,7 +124,10 @@ export class NotaService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} nota`;
+    return this.notaRepo.findOne({
+      where: {id},
+      relations: ['movimientos']
+    });
   }
 
   update(id: number, updateNotaDto: UpdateNotaDto) {
